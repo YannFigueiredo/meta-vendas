036a7d568774ac92ae00685acc65d85eebc4f757
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-08-02 21:00:33 -0300

    Criado projeto backend

---
+++ backend/src/main/java/com/yannfigueiredo/metavendas/MetavendasApplication.java
@@ -0,0 +1,13 @@
+package com.yannfigueiredo.metavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetavendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yannfigueiredo/metavendas/MetavendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yannfigueiredo.metavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetavendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
